110f8e21d0703b7f4c555c5f8c992de11db5fca6
Andrew Barefoot <apolloman3@gmail.com>
2011-12-25 02:19:49 -0500

    Adding UI project

---
+++ TaxRateSolver/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using ViewModel;
+
+namespace TaxRateSolver
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow mw = new MainWindow() { DataContext = new ApplicationViewModel() };
+            mw.Show();
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ TaxRateSolver/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaxRateSolver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private BindingExpression _bindingExpression;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            inputs.Focus();
+            Loaded -= OnLoaded;
+        }
+
+        private void OnEnterNewEarningsKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                if (_bindingExpression != null)
+                {
+                    _bindingExpression.UpdateSource();
+                }
+                else
+                {
+                    TextBox tb = sender as TextBox;
+                    if (tb != null)
+                    {
+                        _bindingExpression = BindingOperations.GetBindingExpression(tb, TextBox.TextProperty);
+                        if (_bindingExpression != null)
+                        {
+                            _bindingExpression.UpdateSource();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
